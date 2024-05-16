0317825f4dbc25dde6fdd7cb38e30cef9d6fe75a
Mateus Dias <dias.mateus@escolar.ifrn.edu.br>
2024-05-15 23:38:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+sudo dnf update -y
 
-sudo apt install gnome-tweaks -y
+sudo dnf install gnome-tweaks -y
 
 if ! command -v flatpak &>/dev/null; then
-    sudo apt install flatpak -y
+    sudo dnf install flatpak -y
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
 flatpak install flathub com.visualstudio.code -y
-
 flatpak install flathub com.jetbrains.PyCharm-Community -y
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo apt install gnome-software-plugin-flatpak -y
+sudo dnf install gnome-software-plugin-flatpak -y
 
 mkdir -p ~/TEMP
 
-gsettings set org.gnome.desktop.interface gtk-theme "ZorinDark"
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 
-wget -O ~/Downloads/wps-office.deb https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10702/wps-office_11.1.0.10702.XA_amd64.deb
-
-sudo dpkg -i ~/Downloads/wps-office.deb
-sudo apt install -f -y
-
-wget -O ~/Downloads/UOS-icons.tar.xz https://dl.opendesktop.org/api/files/download/id/1583525228/s/153aa4a2340c6078913ac25d51d6765a/t/1614243817/u//UOS-icons.tar.xz
-tar -xf ~/Downloads/UOS-icons.tar.xz -C ~/.icons
+wget -O ~/Downloads/wps-office.rpm https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/download/linux/10702/wps-office-11.1.0.10702-1.x86_64.rpm
+sudo dnf install ~/Downloads/wps-office.rpm -y
 
 flatpak install flathub org.gimp.GIMP -y
 
-wget -O ~/Downloads/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ~/Downloads/google-chrome.deb
-sudo apt install -f -y
+wget -O ~/Downloads/google-chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf install ~/Downloads/google-chrome.rpm -y
 
-sudo apt install chrome-gnome-shell -y
+sudo dnf install chrome-gnome-shell -y
 
-rm -rf ~/Downloads/.deb ~/Downloads/.tar.xz
+mkdir -p ~/.local/share/fonts
+wget -O ~/Downloads/Inter.zip https://fonts.google.com/download?family=Inter
+unzip ~/Downloads/Inter.zip -d ~/.local/share/fonts
+fc-cache -f -v
 
-sudo apt autoremove -y
-sudo apt clean
+gsettings set org.gnome.desktop.interface font-name "Inter 11"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Inter Bold 11"
 
-echo "Instalação concluída!"
+rm -f ~/Downloads/*.rpm ~/Downloads/*.tar.xz ~/Downloads/*.zip
+
+sudo dnf autoremove -y
+sudo dnf clean all
+
+echo "Instalação concluída!"
